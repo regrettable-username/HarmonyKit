5cf54c12c12a41ae642bfff756561232322f4569
James Thompson <build.debug.profit@gmail.com>
2021-07-03 16:51:43 -0700

    Converted project to swift package.

---
+++ Tests/HarmonyKitTests/HarmonyKitTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import HarmonyKit
+
+    final class HarmonyKitTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(HarmonyKit().text, "Hello, World!")
+        }
+    }
